0871d1b09b8c22c73c81b90e9eed5c5423f549eb
janesh-git <117098720+janesh-git@users.noreply.github.com>
2023-02-27 21:24:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title(":blue[WELCOME!!]")
+
+st.title(":red[INNOMATICS] DATA SCIENCE INTERN")
+
+st.header("Best Movies of :red[Netflix]")
+
+CODE = '''def hello(): 
+        print("By-Janeshsrinivas")'''
+st.code(CODE,language="python")
---
+++ pages/app..py
@@ -0,0 +1,30 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "image", "image.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "netflix.csv")
+
+st.title("Dashboard - Best_Movies_of_Year_:red[Netflix]")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+col1 , col2 = st.columns(2)
+fig_1 = px.histogram(df["RELEASE_YEAR"])
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.scatter(df,x="TITLE",y="RELEASE_YEAR")
+col2.plotly_chart(fig_2, use_container_width=True)
